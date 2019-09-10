24b8b1e07be083cd06f08915f2bf670b914222fb
boris <bbi220602@gmail.com>
2019-09-10 16:43:27 +0100

    test to push branch

--- JS_Full/003/Home work Js/hwJs.js
+++ JS_Full/003/Home work Js/hwJs.js
@@ -9,10 +9,26 @@ var appData =
         income : [],
         saving : false
     };
-let a1 = prompt('Введите обязательную статью расходов в этом месяце');
-    a2 = prompt('Во сколько обойдется?');
-    a3 = prompt('Введите обязательную статью расходов в этом месяце');
-    a4 = prompt('Во сколько обойдется?');
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+// let a1 = prompt('Введите обязательную статью расходов в этом месяце');
+//     a2 = prompt('Во сколько обойдется?');
+//     a3 = prompt('Введите обязательную статью расходов в этом месяце');
+//     a4 = prompt('Во сколько обойдется?');
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;
+
+for (let i = 0; i < 2; i++)
+{
+    let a = prompt('Введите обязательную статью расходов в этом месяце');
+        b = prompt('Во сколько обойдется?');
+        q = 0;
+    while (q < 1)
+    {
+        if ((typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(b)) !== null
+        && a != '' && b != '' && a.length < 50) 
+        {
+            q++
+            appData.expenses[a] = b;
+        }
+    }
+}
 alert(appData.moneyData / 30);
