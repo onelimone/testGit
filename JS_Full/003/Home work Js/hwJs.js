4c8162638e224fe68002c5742a32537a892ffc59
boris <bbi220602@gmail.com>
2019-09-11 16:43:15 +0100

    github desk

--- JS_Full/003/Home work Js/hwJs.js
+++ JS_Full/003/Home work Js/hwJs.js
@@ -26,7 +26,8 @@ for (let i = 0; i < 2; i++)
         a != '' && b != '' && a.length < 50) 
         {
             appData.expenses[a] = b;
-            console.log(appData[expenses[i]]);
+            console.log(a);
+            console.log(appData.expenses[a]);
             q++;
         } else
           {
--- JS_Full/003/wlileInFor.js
+++ JS_Full/003/wlileInFor.js
@@ -1,11 +1,47 @@
-for(let i = 0; i < 3; i++)
-{
-    let x = '';
-        m = i;
-    while(m < (5 + i));
-    {
-        x = x + m;
-        m++;
+// for(let i = 0; i < 3; i++)
+// {
+//     let x = '';
+//         m = i;
+//     console.log(m);
+//     while(m < (5 + i))
+//     {
+//         x = x + m;
+//         m++;
+//     }
+//     console.log(x);
+// }
+// for(let i = 0; i < 2; i++)
+// {
+//     let a = prompt(1);
+//         b = +prompt(2);
+//         q = 0;
+//     while(q <1)
+//     {
+//         if (/*a.length < 50 && a === 'string' && */a != null &&
+//             b != null)
+//         {
+//             console.log(" " + 1 + " ");
+//             q++;
+//             console.log(" " + 1 + " ");
+//         }   else 
+//             {
+//                 a = prompt(1);
+//                 b = prompt(2);
+//             }
+//     }
+// }
+for(let i = 0; i < 2; ++i){
+    do{
+        let a = prompt(i + "1");
+            b = prompt(i + "2");
+            q = 0;
+
+            console.log('a = "' + a + '", b = "' + b + '"');
+
+            if (a != '' && b != '')
+            {
+                q++;
+            } 
     }
-    console.log(x);
+    while(q <1);
 }
